398cde6983fcf4a51b84b6283a22668fba35702b
Senritsu420 <kubo3900@gmail.com>
2025-02-08 15:51:47 +0900

    infraにスクレイピング処理を移植

--- backend/infrastructure/stock.go
+++ backend/infrastructure/stock.go
@@ -1,6 +1,12 @@
 package infrastructure
 
-import "domestic-stock-checker/domain/repository"
+import (
+	"domestic-stock-checker/domain/repository"
+	"fmt"
+	"log"
+
+	"github.com/gocolly/colly/v2"
+)
 
 type stockPersistence struct{}
 
@@ -9,5 +15,40 @@ func NewStockPersistence() repository.StockRepository {
 }
 
 func (sp *stockPersistence) FetchStockInfo(secuririesCode string) (string, error) {
+	c := colly.NewCollector()
+
+	c.OnHTML("title", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
+	})
+
+	// 「table」タグ内の HTML を処理
+	c.OnHTML("table", func(e *colly.HTMLElement) {
+		// テーブル内の各行（tr）を処理
+		e.ForEach("tr", func(_ int, row *colly.HTMLElement) {
+			var cells []string
+			// ヘッダーセル（th）を取得
+			row.ForEach("th", func(_ int, cell *colly.HTMLElement) {
+				cells = append(cells, cell.Text)
+			})
+			// 通常セル（td）を取得
+			row.ForEach("td", func(_ int, cell *colly.HTMLElement) {
+				cells = append(cells, cell.Text)
+			})
+			// 行ごとにセル内容を表示
+			fmt.Println(cells)
+		})
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// エラーハンドラ
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	c.Visit("https://irbank.net/E04425/results")
 	return "", nil
 }
